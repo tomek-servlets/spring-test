8d49bf5363fd4079ef1693f477c5bb53f82607c7
tomek-servlets <tomek.servlets@gmail.com>
2018-02-27 22:22:28 +0100

    first commit

---
+++ src/main/java/lu/lllc/boottest/Test4Application.java
@@ -0,0 +1,12 @@
+package lu.lllc.boottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test4Application.class, args);
+	}
+}
---
+++ src/main/java/lu/lllc/boottest/controller/MainController.java
@@ -0,0 +1,13 @@
+package lu.lllc.boottest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("/")
+	public String hello(){
+		return "hello";
+	}
+}
